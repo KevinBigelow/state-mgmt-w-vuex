f3412ae7fc4535650672a9965ebd6aba911d4286
Kevin Bigelow <kbigelow5@gmail.com>
2020-07-28 17:16:57 -0600

    initial commit

---
+++ sans/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import('../views/Cart.vue')
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: () => import('../views/Product.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ sans/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <cart></cart>
+  </div>
+</template>
+
+<script>
+import cart from '@/components/Cart.vue'
+
+export default {
+  name: 'Home',
+  components: [cart]
+}
+</script>
